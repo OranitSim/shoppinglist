4ed646db5a4c13c5a962706fc74bf9af5f352890
Gal Yogev <galyogev5@gmail.com>
2021-01-09 18:32:10 +0200

    Shopping list action buttons and footer components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Footer from './components/Footer.jsx';
+import ListActions from './pages/shoppingList/ListActions.jsx';
+import { COMPLETED, DELETE } from './utils/enums'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Good job Oranit!!</h1>
-    </div>
-  );
+export default class App extends Component {
+
+  onAction = action => {
+    console.log('Action:', action);
+    switch (action) {
+      case COMPLETED:
+        console.log('List is completed!!');
+        break;
+      case DELETE:
+        console.log('Deleting list....');
+        break;
+      default:
+        console.error('Uknown action...');
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Good job Oranit!!</h1>
+        <ListActions onAction={this.onAction} />
+        <Footer />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/pages/shoppingList/ListActions.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { COMPLETED, DELETE } from '../../utils/enums';
+import '../../style/pages/shoppingList.css';
+
+export default class ListActions extends Component {
+    state = {
+        buttons: [
+            { 
+                title: 'Delete',
+                onClick: () => this.props.onAction(DELETE),
+                color: '#ff0000',
+                hoverColor: '#fe8686',
+                isHovered: false
+            },
+            { 
+                title: 'Completed',
+                onClick: () => this.props.onAction(COMPLETED),
+                color: '#4caf50',
+                hoverColor: '#b1eab4',
+                isHovered: false
+            }
+        ]
+    }
+
+    toggleHoverState = title => {
+        this.setState({ buttons: this.state.buttons.map(btn => {
+            if (btn.title === title) btn.isHovered = !btn.isHovered
+            return btn;
+        }) });
+    }
+
+    render() {
+        return (
+            <div className="list-actions-container">
+                {this.state.buttons.map((btn, idx) => {
+                    return <button onClick={btn.onClick} key={idx} style={{backgroundColor: btn.isHovered ? btn.hoverColor : btn.color}} className="action-btn" onMouseOver={this.toggleHoverState.bind(this, btn.title)}
+                    onMouseLeave={this.toggleHoverState.bind(this, btn.title)}>{btn.title}</button>
+                })}
+            </div>
+        )
+    }
+}
